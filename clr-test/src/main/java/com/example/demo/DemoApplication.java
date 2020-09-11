d428f5a81a52c8203d3ca21f7a24e4874a00c24e
ibonny <ibonny@gmail.com>
2020-09-11 12:48:56 +0000

    Initial commit with clr-test.

---
+++ clr-test/src/main/java/com/example/demo/DemoApplication.java
@@ -0,0 +1,18 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import java.lang.Override;
+
+@SpringBootApplication
+public class DemoApplication extends CommandLineRunner {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DemoApplication.class, args);
+    }
+
+    @Override
+    
+
+}
